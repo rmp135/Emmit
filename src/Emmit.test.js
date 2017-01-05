cc438d70c9f2850e73d2cec3d1ce051364d1cfbc
Ryan Poole <rmp135@gmail.com>
2017-01-05 19:17:57 +0000

    Tidied tests.

--- src/Emmit.test.js
+++ src/Emmit.test.js
@@ -1,13 +1,15 @@
 import Emmit from './Emmit.js'
 
 describe("Emmit", () => {
-  let e;
+  let e, mock, mockArg1, mockArg2;
   beforeEach(() => {
     e = new Emmit();
+    mock = jest.fn();
+    mockArg1 = {};
+    mockArg2 = [];
   });
   describe("once", () => {
     it("should add the function to the events list", () => {
-      const mock = jest.fn();
       e.once("event", mock);
       expect(e.events.has("event")).toBe(true);
       expect(e.events.get("event").length).toBe(1);
@@ -15,13 +17,11 @@ describe("Emmit", () => {
       expect(e.events.get("event")[0].once).toBe(true);
     });
     it("should remove the function after triggering", () => {
-      const mock = jest.fn();
       e.once("event", mock);
       e.emit("event");
       expect(e.events.size).toBe(0);
     });
     it("should only trigger once", () => {
-      const mock = jest.fn();
       e.once("event", mock);
       e.emit("event");
       expect(mock).toHaveBeenCalled();
@@ -32,7 +32,6 @@ describe("Emmit", () => {
   });
   describe("on", () => {
     it("should add an event to the event list", () => {
-      const mock = jest.fn();
       e.on('event', mock);
       expect(e.events.size).toBe(1);
       expect(e.events.has('event')).toBe(true);
@@ -40,9 +39,6 @@ describe("Emmit", () => {
       expect(e.events.get('event')[0].fn).toBe(mock);
     });
     it("should trigger multiple times", () => {
-      const mock = jest.fn();
-      const mockArg1 = {};
-      const mockArg2 = [];
       e.on("event", mock);
       e.emit("event", mockArg1);
       e.emit("event", mockArg2);
@@ -53,14 +49,16 @@ describe("Emmit", () => {
       expect(mock.mock.calls[1][0]).toBe(mockArg2);
     });
     it("should trigger an event with multiple arguments", () => {
-      const mock = jest.fn();
-      const mockArg1 = {};
-      const mockArg2 = [];
       e.on("event", mock);
       e.emit("event", mockArg1, mockArg2);
       expect(mock.mock.calls.length).toBe(1);
       expect(mock.mock.calls[0][0]).toBe(mockArg1);
       expect(mock.mock.calls[0][1]).toBe(mockArg2);
-    })
+    });
+    // it("should trigger through a regular expression", () => {
+    //   const mock = jest.fn();
+    //   e.on("event", mock);
+    //   e.emit(/.*/, mockArg1)
+    // })
   });
 });
